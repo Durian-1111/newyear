6ff609d15ac8d16e6961e1e1a1abe9d79df1dd13
Bill <79068518+Durian-1111@users.noreply.github.com>
2025-01-18 10:09:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const myvideo = document.getElementById("video");
-const str = '祝你前程似锦\n带我赚大钱';
+const str = '祝你顺利上岸！上岸！上岸！\n一起打很多很多球';
 let i = 0;
 const divTyping = document.getElementById('wish');
 console.log(divTyping, 'divTyping');
